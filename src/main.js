5a0c66dfc6d02a03d36da34a5562ce644313bab8
StarryNight <doleful1314@me.com>
2018-12-06 16:49:29 +0800

    添加fly

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from '@/App'
 import store from '@/store'
+const Fly = require("flyio/dist/npm/wx")//引入
+const fly = new Fly
+Vue.prototype.$fly = fly //挂载到vue原型上
 Vue.prototype.$store = store //挂载到vue原型上
 Vue.config.productionTip = false
-
 const app = new Vue({
   mpType: 'app',
   store,
